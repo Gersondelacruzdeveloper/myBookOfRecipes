b2f5264915123756d9306a7d6ff3c364d9485f2a
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-06-23 15:38:05 +0100

    Add recipes form functionality and password hash for login and sign up

--- myonlinerecipes/views.py
+++ myonlinerecipes/views.py
@@ -1,11 +1,13 @@
+
 from curses import flash
+from curses.ascii import isctrl
 from myonlinerecipes import app
 from myonlinerecipes import db
 from flask import render_template, request, redirect, url_for, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import date
 from myonlinerecipes.models import Recipes
-from .models import User
+from .models import User, Recipes
 
 # Home function 
 @app.route("/")
@@ -26,8 +28,41 @@ def myrecipes():
     return render_template("myrecipes.html", recipes=recipes)
 
 # my recipes form function  
-@app.route("/myrecipes_form")
+@app.route("/myrecipes_form", methods=["GET", "POST"])
 def myrecipes_form():
+
+    user = User.query.filter_by(username=session["user"]).first()
+    if request.method == "POST":
+
+        service_size = request.form.get('service_size') or 0
+        cooking_time = request.form.get('cooking_time') or 0
+        switch = request.form.get('switch')
+
+        if switch:
+            switch = True
+
+        new_recipe = Recipes(
+            user = user.id,
+            title = request.form.get('title'),
+            image_url = request.form.get('image'),
+            Ingredients = request.form.get('ingredients'),
+            method =  request.form.get('method'),
+            service_size = service_size,
+            cooking_time = cooking_time,
+            is_private = switch,
+            date_created = date.today(),
+            calories = request.form.get('calories'),
+            fat = request.form.get('fat'),
+            protein = request.form.get('protein'),
+            carbohidrates = request.form.get('carbohidrates'),
+            salt = request.form.get('salt'),
+        )
+         # add the new recipe to the database
+        db.session.add(new_recipe)
+        db.session.commit()
+
+        flash("Recipe created Succesfullly")
+        return redirect(url_for('myrecipes'))
     return render_template("myrecipes_form.html")
 
 # logout function 
@@ -49,8 +84,8 @@ def sign_up():
     if request.method == "POST":
          #check if user already exist
         existing_user = User.query.filter_by(username=username).first()
+        #check if email already exist
         existing_user_email = User.query.filter_by(email=email).first()
-        # generate_password_hash(password, method='sha256')
         if existing_user:
             flash("User already exits")
             return redirect(url_for("sign_up"))
@@ -66,7 +101,7 @@ def sign_up():
             new_user = User(
             email = email, 
             username = username,
-            password = password,
+            password = generate_password_hash(password, method='sha256'),
             created_on = today, 
         )
         # add the new user to the database
@@ -83,16 +118,14 @@ def login():
     username = request.form.get('username')
     if request.method == "POST":
         username = request.form.get('username')
-        password = request.form.get('password')
         #check if user already exist
         existing_user = User.query.filter_by(username=username).first()
-        
+
         if existing_user:
             # check_password_hash
             #ensure hashed password matches user imput
-            if existing_user.password == password:
+            if check_password_hash(existing_user.password, request.form.get("password")):
                 session["user"] = username
-                flash("Welcome")
                 return redirect(url_for("home"))
             else:
                 #invalid password match
