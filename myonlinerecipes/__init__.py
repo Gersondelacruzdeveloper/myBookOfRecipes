df1608495ed695bff5dda44bdad9dec9480cc5a0
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-06-28 23:54:02 +0100

    Add email info and delete comment

--- myonlinerecipes/__init__.py
+++ myonlinerecipes/__init__.py
@@ -2,10 +2,21 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
+from flask_mail import Mail
 if os.path.exists("env.py"):
     import env # noqa
 
+
 app = Flask(__name__)
+
+# configuration for emails
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = os.environ.get('MAIL_PORT')
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+mail = Mail(app)
 
 # Add ckeditor
 ckeditor = CKEditor(app)
@@ -18,9 +29,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 from myonlinerecipes import views  # noqa
-
-
-# from myonlinerecipes.models import User
-# db.drop_all()
-# db.create_all()
-# db.session.commit()
