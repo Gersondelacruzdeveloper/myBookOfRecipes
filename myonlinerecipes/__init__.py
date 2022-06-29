94cc0ed4253fee08fc6b3b3a76f524acec1d8a69
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-06-29 01:26:28 +0100

    format init file

--- myonlinerecipes/__init__.py
+++ myonlinerecipes/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor
 from flask_mail import Mail
 if os.path.exists("env.py"):
-    import env # noqa
+    import env  # noqa
 
 
 app = Flask(__name__)
@@ -24,7 +24,7 @@ ckeditor = CKEditor(app)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-#Help not to show the SQLALCHEMY_TRACK_MODIFICATIONS message
+# Help not to show the SQLALCHEMY_TRACK_MODIFICATIONS message
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
