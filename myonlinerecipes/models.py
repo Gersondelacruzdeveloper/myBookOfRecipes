9a59b1953a0a1c4f5487a5f9a255fef8388d9c44
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-06-29 01:24:51 +0100

    format models for pp8

--- myonlinerecipes/models.py
+++ myonlinerecipes/models.py
@@ -1,8 +1,9 @@
 
 from myonlinerecipes import db
 
+
 class User(db.Model):
-    #schema for User model
+    # schema for User model
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -13,44 +14,46 @@ class User(db.Model):
     profile_image_url = db.Column(db.String(300), unique=True, nullable=True)
     recipes = db.relationship('Recipes', backref='owned_user_recipes')
     comments = db.relationship('Comments', backref='owned_user_comment')
-  
+
     def __repr__(self):
         return self.username
 
-class Recipes(db.Model):
-    #schema for Recepes model
-     __tablename__ = 'Recipes'
-     id = db.Column(db.Integer, primary_key=True)
-     user = db.Column(db.Integer, db.ForeignKey(User.id, ondelete="CASCADE"), nullable=False)
-     title = db.Column(db.String(201),  nullable=False)
-     image_url = db.Column(db.Text, nullable=True)
-     Ingredients = db.Column(db.Text,  nullable=True)
-     method = db.Column(db.Text,  nullable=True)
-     service_size = db.Column(db.Integer, nullable=True)
-     cooking_time = db.Column(db.Integer, nullable=True)
-     is_private = db.Column(db.Boolean, default=False, nullable=False)
-     date_created = db.Column(db.Date, nullable=False)
-     calories = db.Column(db.String(201),  nullable=True)
-     fat = db.Column(db.String(201), nullable=True)
-     protein = db.Column(db.String(201), nullable=True)
-     carbohidrates = db.Column(db.String(201), nullable=True)
-     salt = db.Column(db.String(201), nullable=True)
-     comments = db.relationship('Comments', backref='comments')
 
-     def __repr__(self):
+class Recipes(db.Model):
+    # schema for Recepes model
+    __tablename__ = 'Recipes'
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey(
+        User.id, ondelete="CASCADE"), nullable=False)
+    title = db.Column(db.String(201),  nullable=False)
+    image_url = db.Column(db.Text, nullable=True)
+    Ingredients = db.Column(db.Text,  nullable=True)
+    method = db.Column(db.Text,  nullable=True)
+    service_size = db.Column(db.Integer, nullable=True)
+    cooking_time = db.Column(db.Integer, nullable=True)
+    is_private = db.Column(db.Boolean, default=False, nullable=False)
+    date_created = db.Column(db.Date, nullable=False)
+    calories = db.Column(db.String(201),  nullable=True)
+    fat = db.Column(db.String(201), nullable=True)
+    protein = db.Column(db.String(201), nullable=True)
+    carbohidrates = db.Column(db.String(201), nullable=True)
+    salt = db.Column(db.String(201), nullable=True)
+    comments = db.relationship('Comments', backref='comments')
+
+    def __repr__(self):
         return self.title
 
 
 class Comments(db.Model):
-     #schema for Recepes model
-     __tablename__ = 'Comments'
-     id = db.Column(db.Integer, primary_key=True)
-     content = db.Column(db.Text, nullable=False)
-     recipes_id = db.Column(db.Integer, db.ForeignKey(Recipes.id, ondelete="CASCADE"))
-     user = db.Column(db.Integer, db.ForeignKey(User.id, ondelete="CASCADE"))
-     created =  db.Column(db.Date, nullable=False)
-     username = db.relationship('User', backref='comment_user')
+    # schema for Recepes model
+    __tablename__ = 'Comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    recipes_id = db.Column(db.Integer, db.ForeignKey(
+        Recipes.id, ondelete="CASCADE"))
+    user = db.Column(db.Integer, db.ForeignKey(User.id, ondelete="CASCADE"))
+    created = db.Column(db.Date, nullable=False)
+    username = db.relationship('User', backref='comment_user')
 
-     def __repr__(self):
+    def __repr__(self):
         return self.title
-
