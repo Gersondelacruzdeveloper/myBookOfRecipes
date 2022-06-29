ca308e6702490fc53aa734c387e4334f3fb7dda2
Gerson De La Cruz <gersondelacruz@Gersons-MacBook-Pro.local>
2022-06-29 01:27:40 +0100

    format script.js

--- myonlinerecipes/static/js/script.js
+++ myonlinerecipes/static/js/script.js
@@ -1,12 +1,12 @@
 /**
  * help the user to understand if the recipes will be private or public
  */
- const privateSwitch =()=>{
+const privateSwitch = () => {
     let myswitch = document.getElementById("switch")
-     if(myswitch.checked === true){
-         document.getElementById("switch-label").innerHTML = 'Private'
-     }else{
-         document.getElementById("switch-label").innerHTML = 'Public'
-     }
- }
- privateSwitch()
+    if (myswitch.checked === true) {
+        document.getElementById("switch-label").innerHTML = 'Private'
+    } else {
+        document.getElementById("switch-label").innerHTML = 'Public'
+    }
+}
+privateSwitch()
